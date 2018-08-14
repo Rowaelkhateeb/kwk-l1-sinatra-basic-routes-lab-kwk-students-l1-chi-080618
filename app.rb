d86c3abd10be14c2fa5a99db4fb07e6e793ced98
Rowaelkhateeb <rowaelkhateeb2001@gmail.com>
2018-08-14 19:03:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
+<<<<<<< HEAD
     "My name is Rowa.
     <br>
     My hometown is Naperville.
     <br>
     My favorite song is Better now."
   end
+=======
+    "My name is Rowa"
+  end
+  get '/' do 
+    "My hometown is Naperville"
+  get '/' do
+    "My favorite song is Better now"
+>>>>>>> 3405d0ba31847f4769ed35b146eb63db2e543f5e
 end
